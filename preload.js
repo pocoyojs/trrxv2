4f82570082d7e60657eb55519c17925513c271d9
hckmemories <112208359+pocoyojs@users.noreply.github.com>
2026-01-05 16:05:34 -0300

    Delete preload.js

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    onUpdateAvailable: (callback) => ipcRenderer.on('update-available', (event, version) => callback(event, version)),
-    onUpdateProgress: (callback) => ipcRenderer.on('download-progress', (event, percent) => callback(event, percent)),
-    onUpdateDownloaded: (callback) => ipcRenderer.on('update-downloaded', (event) => callback(event)),
-    onUpdaterError: (callback) => ipcRenderer.on('updater-error', (event, message) => callback(event, message))
-});
